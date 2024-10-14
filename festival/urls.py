1a4e4b6356ecc76a183f8f915ee6e1ace4a99518
smrha <lord.smrha@gmail.com>
2024-10-14 07:45:04 +0330

    initial

--- festival/urls.py
+++ festival/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
     path('', include('article.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
