cb15f89f442673cb27d3220e58a220e900eee68b
smrha <lord.smrha@gmail.com>
2024-10-27 00:30:00 +0330

    Edit carousel

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -23,7 +24,7 @@ class Post(models.Model):
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
     lead = models.TextField()
-    body = models.TextField()
+    body = RichTextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- festival/settings.py
+++ festival/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_jalali',
+    'ckeditor',
     'blog',
     'account'
 ]
