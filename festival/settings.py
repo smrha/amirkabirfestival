d278a7cf533193a0ef4e6d60963c340b057fce28
smrha <lord.smrha@gmail.com>
2024-10-28 01:45:11 +0330

    collectstatic

--- festival/settings.py
+++ festival/settings.py
@@ -115,11 +115,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
